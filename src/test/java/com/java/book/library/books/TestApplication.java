be148417a5369472b15b75b5c1f108bf288ccd1e
serhad <serhad1@outlook.de>
2025-01-31 19:52:00 +0100

    init

---
+++ src/test/java/com/java/book/library/books/TestApplication.java
@@ -0,0 +1,11 @@
+package com.java.book.library.books;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(Application::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
