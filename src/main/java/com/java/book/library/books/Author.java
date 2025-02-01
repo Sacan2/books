f18901dd6db03af2aa3d6d37b4f57002a908cb7f
serhad <serhad1@outlook.de>
2025-02-01 12:21:56 +0100

    Kontruktor für Author erstellt

---
+++ src/main/java/com/java/book/library/books/Author.java
@@ -0,0 +1,16 @@
+package com.java.book.library.books;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table
+public class Author {
+    private String name;
+    private String dateOfBirth;
+    private int releasedBooks;
+}
